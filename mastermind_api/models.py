72469c1fe1e4163c236ff8435e5ded860635508c
J. Victor Martins <jvdm@sdf.org>
2016-05-21 00:25:25 -0300

    Initial Game model and model api

--- mastermind_api/models.py
+++ mastermind_api/models.py
@@ -1,3 +1,27 @@
+import random
+
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    """A mastermind game."""
+
+    COLORS = ('red', 'orange', 'yellow', 'blue',
+              'purple', 'brown', 'gray', 'magenta',
+              'mahogany', 'peach', 'pink', 'mango')
+
+    created_at = models.DateTimeField(
+        auto_now_add=True)
+
+    modified_at = models.DateTimeField(
+        auto_now=True)
+
+    secret = models.CharField(
+        editable=False,
+        max_length=256)
+
+    def save(self, *args, **kwds):
+        if self.pk is None:
+            self.secret = ','.join(random.choice(self.COLORS)
+                                   for _ in range(0, 8))
+        super().save(*args, **kwds)
--- mastermind_api/views.py
+++ mastermind_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Game
+from .serializers import GameSerializer
+
+
+class GameViewSet(ModelViewSet):
+    """This viewset automatically provides `list` and `detail` actions."""
+
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
