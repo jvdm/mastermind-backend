0d1c079c229bdac1f164724cafa739bf6fa6d788
Felipe Nunes Franco <felipenfranco@gmail.com>
2016-05-21 12:06:29 -0300

    Add mastermind engine and tests

---
+++ mastermind_api/mastermind_engine.py
@@ -0,0 +1,25 @@
+from itertools import compress
+
+
+class MastermindGameEngine:
+
+    def __init__(self, secret):
+        self.secret = secret
+
+    def evaluate_guess(self, guess):
+        assert len(guess) == len(self.secret)
+        correct_num = sum((map((lambda x, y: x == y), self.secret, guess)))
+        incorrect_filter = list(map((lambda x, y: x != y), self.secret, guess))
+        remaining_secret = list(compress(self.secret, incorrect_filter))
+        incorrect_guesses = list(compress(guess, incorrect_filter))
+
+        incorrect_guesses_num = 0
+        for g in incorrect_guesses:
+            if g in remaining_secret:
+                remaining_secret.remove(g)
+                incorrect_guesses_num += 1
+
+        return correct_num, incorrect_guesses_num
+
+
+
--- mastermind_api/tests.py
+++ mastermind_api/tests.py
@@ -1,3 +1,20 @@
+from .mastermind_engine import MastermindGameEngine
 from django.test import TestCase
 
-# Create your tests here.
+
+class MastermindGameEngineTest(TestCase):
+
+    def test_evaluate_guess(self):
+        secret = "ABCDEFGH"
+        ge = MastermindGameEngine(secret)
+        result = ge.evaluate_guess("ABCDAAAA")
+        self.assertEqual(result, (4, 0))
+
+        result = ge.evaluate_guess("ABCDAAAE")
+        self.assertEqual(result, (4, 1))
+
+        result = ge.evaluate_guess(secret)
+        self.assertEqual(result, (len(secret), 0))
+
+        result = ge.evaluate_guess(list(reversed(secret)))
+        self.assertEqual(result, (0, len(secret)))
