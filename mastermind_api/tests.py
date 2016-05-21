020f754bcd4b8f28f6c5806090e54ea91d5f8b21
J. Victor Martins <jvdm@sdf.org>
2016-05-21 17:20:20 -0300

    Add API (broken) tests

--- mastermind_api/tests.py
+++ mastermind_api/tests.py
@@ -1,5 +1,11 @@
-from .mastermind_engine import MastermindGameEngine
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from .mastermind_engine import MastermindGameEngine
+from .models import Game
 
 
 class MastermindGameEngineTest(TestCase):
@@ -18,3 +24,21 @@ class MastermindGameEngineTest(TestCase):
 
         result = ge.evaluate_guess(list(reversed(secret)))
         self.assertEqual(result, (0, len(secret)))
+
+
+class APITestCase(APITestCase):
+
+    def test_should_create_game(self):
+        resp = self.client.post(reverse('game-list'),
+                                {'player_count': 1},
+                                format='json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Game.objects.count(), 1)
+
+    def test_should_wait_players_to_join(self):
+        players = 5
+        resp = self.client.post(reverse('game-list'),
+                                {'player_count': players},
+                                format='json')
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Game.objects.count(), 1)
